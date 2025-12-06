4287aece4dff3d4016d95d5b314eba672239bb81
milon <milon.miah@qq.com>
2025-12-07 06:26:41 +0800

    modified package.json file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
-
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 
+
+app.use(cors({
+    origin: "*"
+}))
 
 // Health Route
 import HealthRoute from "./modules/health/routes/health.routes";
